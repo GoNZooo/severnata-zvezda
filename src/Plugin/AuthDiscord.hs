0063b9388589a3b9c2b7dd9fd8f9cc690536b74b
Rickard Andersson <gonz@severnatazvezda.com>
2021-04-02 19:47:47 +0000

    add beginnings of a login widget

--- src/Plugin/AuthDiscord.hs
+++ src/Plugin/AuthDiscord.hs
@@ -1,15 +1,10 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Plugin.AuthDiscord where
@@ -18,28 +13,57 @@ import ClassyPrelude.Yesod
 import Data.Aeson.Types (Result (..), parseMaybe, withObject)
 import Yesod.Auth
 
-class (YesodAuth site, FromJSON (RequestId site)) => AuthDiscord site where
+type Form x site = Html -> MForm (HandlerFor site) (FormResult x, WidgetFor site ())
+
+newtype FormUsername = FormUsername Text
+
+class (YesodAuth site, FromJSON (RequestId site), (RenderMessage site Text)) => AuthDiscord site where
   type RequestId site
   type LoginUser site
   isLoginRequestApproved :: RequestId site -> HandlerFor site Bool
   getUserForLoginRequest :: RequestId site -> HandlerFor site (Maybe (LoginUser site))
   encodeUser :: LoginUser site -> Text
 
-authDiscord :: (AuthDiscord site) => AuthPlugin site
+authDiscord :: forall site. (AuthDiscord site) => AuthPlugin site
 authDiscord =
   let dispatch "POST" [] = authDiscordPost >>= sendResponse
       dispatch _ _ = notFound
-      login _authToMaster = do
-        _request <- getRequest
-        toWidget [hamlet|<h2>Hello|]
+      loginForm extra = do
+        (usernameResult, usernameView) <- mreq textField (fieldSettings "Username") Nothing
+        let loginWidget = do
+              [whamlet|
+                  #{extra}
+                  <div>
+                      ^{fvInput usernameView}
+              |]
+        pure (usernameResult, loginWidget)
+      -- @TODO: add login route that does the waiting for response that we do via the user routes now.
+      -- This can be done via the second argument to `dispatch` which is basically a split path.
+      login authToMaster = do
+        (formWidget, formEncodingType) <- generateFormPost loginForm
+        [whamlet|
+            <form method=post action=@{authToMaster $ PluginR "discord" []} enctype=#{formEncodingType}>
+                <div id="discord-login">
+                    ^{formWidget}
+                <button .btn>Login via Discord
+        |]
+      fieldSettings :: Text -> FieldSettings site
+      fieldSettings label =
+        FieldSettings
+          { fsLabel = SomeMessage label,
+            fsTooltip = Nothing,
+            fsId = Nothing,
+            fsName = Nothing,
+            fsAttrs = []
+          }
    in AuthPlugin "discord" dispatch login
 
 authDiscordPost :: forall site. (AuthDiscord site) => AuthHandler site TypedContent
 authDiscordPost = do
   let parseLoginRequestIdBody = withObject "loginRequestIdBody" $ \o -> do
         o .: "loginRequestId"
-  maybeJsonContent :: Result Value <- parseCheckJsonBody
-  case maybeJsonContent of
+  maybeJsonValue <- parseCheckJsonBody
+  case maybeJsonValue of
     Success jsonContent -> do
       case parseMaybe parseLoginRequestIdBody jsonContent of
         Just loginRequestId' -> do
