da00736411a3cf7b18ded08761f2cf11b4ca9b14
Rickard Andersson <gonz@severnatazvezda.com>
2021-04-05 20:00:36 +0000

    add tokens for services
    Only a service with a valid token can authenticate

--- src/Database.hs
+++ src/Database.hs
@@ -20,7 +20,8 @@ import Model
 data ChallengeApprovalPayload = ChallengeApprovalPayload
   { uuid :: UUID,
     username :: Text,
-    discordUsername :: Text
+    discordUsername :: Text,
+    token :: Token
   }
   deriving (Eq, Show, Generic, FromJSON)
 
@@ -29,36 +30,44 @@ appApproveLoginRequest
   ChallengeApprovalPayload
     { uuid = uuid',
       username = username',
-      discordUsername =
-        discordUsername'
+      discordUsername = discordUsername',
+      token = Token {tokenService = service', tokenKey = key'}
     } = do
-    matches <- select $ do
-      (users :& loginRequests) <-
-        from $
-          Table @User `InnerJoin` Table @LoginRequest
-            `on` ( \(users :& loginRequest) ->
-                     users ^. UserId ==. loginRequest ^. LoginRequestUserId
-                 )
-      where_
-        ( users ^. UserUsername ==. val username'
-            &&. loginRequests ^. LoginRequestUuid ==. val uuid'
-        )
-      pure (users, loginRequests)
-    currentTime <- liftIO getCurrentTime
-    case matches of
-      [ ( Entity _userId' User {userDiscordUsername = Just existingDiscordUsername},
-          Entity loginRequestId' _loginRequest
-          )
-        ]
-          | existingDiscordUsername == discordUsername' -> do
-            Persist.update loginRequestId' [LoginRequestApproved Persist.=. Just currentTime]
-            pure True
-      [ ( Entity userId' User {userDiscordUsername = Nothing},
-          Entity loginRequestId' _loginRequest
-          )
-        ] -> do
-          Persist.update loginRequestId' [LoginRequestApproved Persist.=. Just currentTime]
-          Persist.update userId' [UserDiscordUsername Persist.=. Just discordUsername']
-          pure True
-      [] -> error "No results"
-      _ -> error "Too many results"
+    maybeToken <- Persist.getBy $ UniqueService service'
+    case maybeToken of
+      Just (Entity _tokenId (Token existingTokenKey existingTokenService))
+        | existingTokenKey == key' && existingTokenService == service' -> do
+          currentTime <- liftIO getCurrentTime
+
+          matches <- select $ do
+            (users :& loginRequests) <-
+              from $
+                Table @User `InnerJoin` Table @LoginRequest
+                  `on` ( \(users :& loginRequest) ->
+                           users ^. UserId ==. loginRequest ^. LoginRequestUserId
+                       )
+            where_
+              ( users ^. UserUsername ==. val username'
+                  &&. loginRequests ^. LoginRequestUuid ==. val uuid'
+              )
+            pure (users, loginRequests)
+
+          case matches of
+            [ ( Entity _userId' User {userDiscordUsername = Just existingDiscordUsername},
+                Entity loginRequestId' _loginRequest
+                )
+              ]
+                | existingDiscordUsername == discordUsername' -> do
+                  Persist.update loginRequestId' [LoginRequestApproved Persist.=. Just currentTime]
+                  pure True
+            [ ( Entity userId' User {userDiscordUsername = Nothing},
+                Entity loginRequestId' _loginRequest
+                )
+              ] -> do
+                Persist.update loginRequestId' [LoginRequestApproved Persist.=. Just currentTime]
+                Persist.update userId' [UserDiscordUsername Persist.=. Just discordUsername']
+                pure True
+            [] -> error "No results"
+            _ -> error "Too many results"
+      Just _token -> error "Invalid token"
+      Nothing -> error "Invalid token"
