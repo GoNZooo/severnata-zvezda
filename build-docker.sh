7c702ea36e2f493e9670431c1eca2d3c6920e692
Rickard Andersson <gonz@severnatazvezda.com>
2021-04-25 09:51:08 +0300

    add pull to build file

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,8 @@
 export APP_NAME="severnata-zvezda"
 export GIT_BRANCH="main"
-export DOCKER_TAG="severnata-zvezda:latest"
+export DOCKER_TAG="gonz/severnata-zvezda:latest"
+
+docker pull fpco/stack-build:lts-16.31
 
 docker-build-cacher build
 docker-build-cacher cache
